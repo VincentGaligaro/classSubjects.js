353a420cdd20eee8f20fdd739bc0c49d6cc5198b
VincentGaligaro <jhangvincent19@gmail.com>
2024-11-09 13:34:15 +0800

    classSubjects.js

---
+++ classSubjects.js
@@ -0,0 +1,7 @@
+let subjectTitle = "History 101";
+let classSchedule = "Monday, Wednesday, and Friday, 8:00 AM - 9:00 AM";
+let classroom = "Room 303";
+let classInstructor = "Mr. Abaya"; 
+let studentName = "Vincet Galigaro"; 
+
+console.log(studentName + " is currently enrolled in " + subjectTitle + " with a class schedule of " + classSchedule + " at room " + classroom + ". The instructor for the subject is " + classInstructor + ".");
